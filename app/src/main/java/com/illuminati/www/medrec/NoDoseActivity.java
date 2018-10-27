c4bea98eebaddce8bbf8980f837eb0bc96b1acfd
Nehal Kumar Singh <nehaljohn91@gmail.com>
2018-10-27 14:42:59 +0530

    NoDoseActivity created

---
+++ app/src/main/java/com/illuminati/www/medrec/NoDoseActivity.java
@@ -0,0 +1,13 @@
+package com.illuminati.www.medrec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoDoseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_dose);
+    }
+}
